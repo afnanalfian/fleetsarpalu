0c26f38c3d0806e8ce6be1439451c1993ce37094
afnanalfian <afnan.02alf@gmail.com>
2025-11-21 22:47:20 +0800

    perbaikan hamburger part 3

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".sidebar-toggler");
     const sidebar = document.getElementById("sidebar");
     const content = document.querySelector(".content");
+    const navbar  = document.querySelector(".navbar");
 
     if (hamburger && sidebar && content) {
         hamburger.addEventListener("click", (e) => {
@@ -10,4 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
             content.classList.toggle('sidebar-open');
         });
     }
+
+    function adjustSidebarPosition() {
+        if (window.innerWidth <= 991.98) {
+            const navHeight = navbar.offsetHeight;
+
+            sidebar.style.top = navHeight + "px";
+            sidebar.style.height = `calc(100vh - ${navHeight}px)`;
+        } else {
+            sidebar.style.top = "0";
+            sidebar.style.height = "100vh";
+        }
+    }
+
+    adjustSidebarPosition();
+    window.addEventListener("resize", adjustSidebarPosition);
 });
