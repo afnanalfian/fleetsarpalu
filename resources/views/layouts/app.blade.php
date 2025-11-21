a55f0296be163a27b10e0da45ca71e97b3ed0c64
afnanalfian <afnan.02alf@gmail.com>
2025-11-21 09:31:03 +0800

    Perbaikan jam di create dan edit peminjaman part 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <title>{{ config('app.name') }} | {{ auth()->user()->role_label }} | @yield('title')</title>
 </head>
 
@@ -23,5 +24,6 @@
 
 <script src="https://kit.fontawesome.com/e814145206.js" crossorigin="anonymous"></script>
 <script src="{{ asset('js/dashboard.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 </body>
 </html>
