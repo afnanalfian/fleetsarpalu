afa7e51291efb725d5f05e8c638526040a8fde43
afnanalfian <afnan.02alf@gmail.com>
2026-04-24 13:47:02 +0800

    additional users part 2

--- database/seeders/AdditionalFlatsarUserSeeder.php
+++ database/seeders/AdditionalFlatsarUserSeeder.php
@@ -16,14 +16,14 @@ class AdditionalFlatsarUserSeeder extends Seeder
         $teams = Team::pluck('id', 'name')->toArray();
 
         $users = [
-            ['LA ODE ADITYA I.F, A.MK', '199112182025061001', 'la.firdaus@basarnas.go.id', '082292812021', 'Anggota Regu', 'ALFA'],
-            ['DWI CAHYO S.', '199606302015031001', 'dwi.setyawan@basarnas.go.id', '085395479190', 'Anggota Regu', 'ALFA'],
-            ['MOH. RIANG FAOZI', '199511032015031001', 'moh.faozi@basarnas.go.id', '082296007471', 'Anggota Regu', 'BRAVO'],
-            ['NURHAYATI', '199304112015032001', 'nurhayati@basarnas.go.id', '082293923124', 'Anggota Regu', 'CHARLIE'],
-            ['MUH. SYAIFULLAH ROSYID H, A.Md.', '199407112025061003', 'muhammad.hasyimi@basarnas.go.id', '082234662851', 'Anggota Regu', 'CHARLIE'],
-            ['ASRUL ARIMANSAR, S.AP', '198608052007121001', 'asrul.arimansar@basarnas.go.id', '085145000022', 'Ketua Regu', 'CHARLIE'],
-            ['AZWAR A.', '198905252010121003', 'azwar@basarnas.go.id', '085243845213', 'Ketua Regu', 'DELTA'],
-            ['MUH. SUGIANTO', '198803122015031003', 'muhammad@basarnas.go.id', '082323667421', 'Anggota Regu', 'ECHO'],
+            ['LA ODE ADITYA I.F, A.MK', '199112182025061001', 'la.firdaus@basarnas.go.id', '082292812021', 'Pegawai', 'ALFA'],
+            ['DWI CAHYO S.', '199606302015031001', 'dwi.setyawan@basarnas.go.id', '085395479190', 'Pegawai', 'ALFA'],
+            ['MOH. RIANG FAOZI', '199511032015031001', 'moh.faozi@basarnas.go.id', '082296007471', 'Pegawai', 'BRAVO'],
+            ['NURHAYATI', '199304112015032001', 'nurhayati@basarnas.go.id', '082293923124', 'Pegawai', 'CHARLIE'],
+            ['MUH. SYAIFULLAH ROSYID H, A.Md.', '199407112025061003', 'muhammad.hasyimi@basarnas.go.id', '082234662851', 'Pegawai', 'CHARLIE'],
+            ['ASRUL ARIMANSAR, S.AP', '198608052007121001', 'asrul.arimansar@basarnas.go.id', '085145000022', 'Ketua Tim', 'CHARLIE'],
+            ['AZWAR A.', '198905252010121003', 'azwar@basarnas.go.id', '085243845213', 'Ketua Tim', 'DELTA'],
+            ['MUH. SUGIANTO', '198803122015031003', 'muhammad@basarnas.go.id', '082323667421', 'Pegawai', 'ECHO'],
         ];
 
         foreach ($users as $u) {
@@ -42,8 +42,8 @@ class AdditionalFlatsarUserSeeder extends Seeder
                 'email_verified_at' => Carbon::now(),
             ]);
 
-            // Jika Ketua Regu → bisa kamu sesuaikan kalau mau jadi leader juga
-            if ($u[4] === 'Ketua Regu' && $teamId) {
+            // Jika Ketua Tim → bisa kamu sesuaikan kalau mau jadi leader juga
+            if ($u[4] === 'Ketua Tim' && $teamId) {
                 Team::where('id', $teamId)->update([
                     'leader_id' => $user->id
                 ]);
